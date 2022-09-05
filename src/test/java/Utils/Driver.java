bfab279cb2703f4dbcb4c7e31eea72c85196e208
alliswell <koray.akcay55@gmail.com>
2022-09-05 13:21:29 +0300

    Driver Class

---
+++ src/test/java/Utils/Driver.java
@@ -0,0 +1,26 @@
+package Utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    public WebDriver getDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+    public void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+}
